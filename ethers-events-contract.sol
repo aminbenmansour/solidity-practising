19a2589a7f43606cd2989bf78f35b3f9017aa88f
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-14 21:16:56 +0100

    transfer ethereum

---
+++ ethers-events-contract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.1;
+
+contract EthersEventsContract {
+
+    mapping(address => uint256) public balances;
+
+    address payable wallet;
+
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+    }
+
+    function buyToken() public payable {
+        // buy token
+        balances[msg.sender] += 1;
+
+        // send ethers to wallet
+        wallet.transfer(msg.value);
+    }
+}

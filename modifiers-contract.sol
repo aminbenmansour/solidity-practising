dd8bac40e113f737accc833465760917e9a724bd
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-14 20:38:46 +0100

    function callable only by owner

--- modifiers-contract.sol
+++ modifiers-contract.sol
@@ -5,12 +5,23 @@ contract ModifiersContract {
     mapping(uint256 => Person) public person;
     uint256 public peopleCount = 0;
 
+    address owner;
+
     struct Person {
         string _firstname;
         string _lastname;
     }
 
-    function addPerson(string memory _firstname, string memory _lastname) public {
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function addPerson(string memory _firstname, string memory _lastname) public onlyOwner {
         person[peopleCount] = Person(_firstname, _lastname);
         peopleCount++;
     }

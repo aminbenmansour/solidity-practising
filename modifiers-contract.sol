1fa60d54e0142d766338b716482427238e595f77
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-14 20:25:50 +0100

    skeleton to changes

---
+++ modifiers-contract.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.1;
+
+contract ModifiersContract {
+
+    mapping(uint256 => Person) public person;
+    uint256 public peopleCount = 0;
+
+    struct Person {
+        string _firstname;
+        string _lastname;
+    }
+
+    function addPerson(string memory _firstname, string memory _lastname) public {
+        person[peopleCount] = Person(_firstname, _lastname);
+        peopleCount++;
+    }
+}

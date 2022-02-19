7c24be3b9891bcee3898c86a373f30026552387d
aminbenmansour <amin.benmansour.10@gmail.com>
2022-02-19 23:19:03 +0100

    import external librairies

--- 07-librairies-contract.sol
+++ 07-librairies-contract.sol
@@ -1,12 +1,6 @@
 pragma solidity ^0.5.1;
 
-library Math {
-    function divide(uint256 a, uint256 b) internal pure returns(uint256) {
-        require(b > 0);
-        uint256 c = a / b;
-        return c;
-    }
-}
+import "./07-Math.sol"
 
 contract Contract {
     string public value;
